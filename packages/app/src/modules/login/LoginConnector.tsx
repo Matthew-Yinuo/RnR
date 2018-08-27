7ba4a068cd96f1e5b777cfaa97ebac94cf61ed1f
Matthew Hoth <matthewhoth@gmail.com>
2018-08-28 00:17:57 +0800

    Update the method of handling cookies app side

--- packages/app/src/modules/login/LoginConnector.tsx
+++ packages/app/src/modules/login/LoginConnector.tsx
@@ -1,18 +1,26 @@
 import * as React from "react";
 import { LoginController } from "@airbnb/controller";
-import { SecureStore } from "expo";
+import { RouteComponentProps } from "react-router-native";
+// import { SecureStore } from "expo";
 import { LoginView } from "./view/LoginView";
-import { SID_KEY } from "../shared/constants";
+// import { SID_KEY } from "../shared/constants";
 
-export class LoginConnector extends React.PureComponent {
-  saveSessionId = (sid: string) => {
-    SecureStore.setItemAsync(SID_KEY, sid);
+export class LoginConnector extends React.PureComponent<
+  RouteComponentProps<{}>
+> {
+  // saveSessionId = (sid: string) => {
+  //   SecureStore.setItemAsync(SID_KEY, sid);
+  // };
+  onFinish = () => {
+    this.props.history.push("/me");
   };
 
   render() {
     return (
-      <LoginController onSessionId={this.saveSessionId}>
-        {({ submit }) => <LoginView submit={submit} />}
+      <LoginController>
+        {({ submit }: { submit: any }) => (
+          <LoginView onFinish={this.onFinish} submit={submit} />
+        )}
       </LoginController>
     );
   }
