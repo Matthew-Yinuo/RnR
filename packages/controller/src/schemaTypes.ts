b455061d1ad2662d14d9ede48fe3541ebc1301ac
Matthew Hoth <matthewhoth@gmail.com>
2018-09-05 23:06:55 +0800

    Export and generate schema

--- packages/controller/src/schemaTypes.ts
+++ packages/controller/src/schemaTypes.ts
@@ -184,6 +184,23 @@ export interface RegisterUserMutationVariables {
 // This file was automatically generated and should not be edited.
 
 // ====================================================
+// GraphQL mutation operation: UpdateListingMutation
+// ====================================================
+
+export interface UpdateListingMutation {
+  updateListing: boolean;
+}
+
+export interface UpdateListingMutationVariables {
+  listingId: string;
+  input: UpdateListingInput;
+}
+
+
+/* tslint:disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
 // GraphQL query operation: ViewListingQuery
 // ====================================================
 
@@ -196,6 +213,13 @@ export interface ViewListingQuery_viewListing {
   id: string;
   name: string;
   category: string;
+  description: string;
+  price: number;
+  beds: number;
+  guests: number;
+  longitude: number;
+  latitude: number;
+  amenities: string[];
   pictureUrl: string;
   owner: ViewListingQuery_viewListing_owner;
 }
@@ -235,6 +259,33 @@ export interface ViewMessagesQueryVariables {
   listingId: string;
 }
 
+
+/* tslint:disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL subscription operation: NewMessageSubscription
+// ====================================================
+
+export interface NewMessageSubscription_newMessage_user {
+  id: string;
+  email: string;
+}
+
+export interface NewMessageSubscription_newMessage {
+  text: string;
+  user: NewMessageSubscription_newMessage_user;
+  listingId: string;
+}
+
+export interface NewMessageSubscription {
+  newMessage: NewMessageSubscription_newMessage;
+}
+
+export interface NewMessageSubscriptionVariables {
+  listingId: string;
+}
+
 /* tslint:disable */
 // This file was automatically generated and should not be edited.
 
@@ -248,6 +299,23 @@ export interface ViewMessagesQueryVariables {
 export interface MessageInput {
   text: string;
   listingId: string;
+}
+
+/**
+ * 
+ */
+export interface UpdateListingInput {
+  name?: string | null;
+  picture?: any | null;
+  pictureUrl?: string | null;
+  category?: string | null;
+  description?: string | null;
+  price?: number | null;
+  beds?: number | null;
+  guests?: number | null;
+  latitude?: number | null;
+  longitude?: number | null;
+  amenities?: string[] | null;
 }
 
 //==============================================================
