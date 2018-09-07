c6fd199b88287fc4c649efd17651cee0f5ce57f7
Matthew Hoth <matthewhoth@gmail.com>
2018-09-07 15:33:16 +0800

    Heroku tweak

--- packages/server/src/startServer.ts
+++ packages/server/src/startServer.ts
@@ -72,7 +72,7 @@ export const startServer = async () => {
       resave: false,
       saveUninitialized: false,
       cookie: {
-        httpOnly: true,
+        httpOnly: false,
         secure: process.env.NODE_ENV === "production",
         maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
       }
