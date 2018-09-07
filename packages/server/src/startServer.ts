c8d042e991faf49abbc00900ec3e5901689407f7
Matthew Hoth <matthewhoth@gmail.com>
2018-09-07 15:52:28 +0800

    Why is it failing on heroku?

--- packages/server/src/startServer.ts
+++ packages/server/src/startServer.ts
@@ -72,7 +72,7 @@ export const startServer = async () => {
       resave: false,
       saveUninitialized: false,
       cookie: {
-        httpOnly: false,
+        httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
       }
