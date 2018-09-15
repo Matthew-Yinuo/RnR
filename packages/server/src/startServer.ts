0d256a89e835ec0dd2c221f96dd87c090f6e6e37
Matthew Hoth <matthewhoth@gmail.com>
2018-09-16 01:10:16 +0800

    Ok one more

--- packages/server/src/startServer.ts
+++ packages/server/src/startServer.ts
@@ -86,7 +86,7 @@ export const startServer = async () => {
 
   const cors = {
     credentials: true,
-    origin: process.env.FRONTEND_HOST as string
+    origin: "*"
   };
 
   server.express.get("/confirm/:id", confirmEmail);
