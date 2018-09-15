bc182132415fe3fe3b264f84ca863dd3339bd3c9
Matthew Hoth <matthewhoth@gmail.com>
2018-09-15 14:41:03 +0800

    Fetch more listings when user scrolls to the bottom of flat list

--- packages/controller/src/controllers/searchListings/index.tsx
+++ packages/controller/src/controllers/searchListings/index.tsx
@@ -35,6 +35,8 @@ export const searchListingsQuery = gql`
 export interface WithSearchListings {
   listings: SearchListingsQuery_searchListings[];
   loading: boolean;
+  loadMore: () => void;
+  hasMoreListings: boolean;
 }
 interface Props {
   variables: SearchListingsQueryVariables;
@@ -48,14 +50,42 @@ export class SearchListings extends React.PureComponent<Props> {
         query={searchListingsQuery}
         variables={variables}
       >
-        {({ data, loading }) => {
+        {({ data, loading, fetchMore }) => {
           let listings: SearchListingsQuery_searchListings[] = [];
           if (data && data.searchListings) {
             listings = data.searchListings;
           }
+
+          let hasMoreListings = listings.length % variables.limit === 0;
+
+          if (listings.length <= variables.offset) {
+            hasMoreListings = false;
+          }
+
           return children({
+            hasMoreListings,
             listings,
-            loading
+            loading,
+            loadMore: () => {
+              fetchMore({
+                variables: {
+                  ...variables,
+                  offset: listings.length
+                },
+                updateQuery: (prev, { fetchMoreResult }) => {
+                  if (!fetchMoreResult) {
+                    return prev;
+                  }
+                  return {
+                    ...prev,
+                    searchListings: [
+                      ...prev.searchListings,
+                      ...fetchMoreResult.searchListings
+                    ]
+                  };
+                }
+              });
+            }
           });
         }}
       </Query>
