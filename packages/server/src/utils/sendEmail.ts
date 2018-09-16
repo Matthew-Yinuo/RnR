3a9e71d25c341b11d3d769dc6acb482f0cf17b0e
Matthew Hoth <matthewhoth@gmail.com>
2018-09-16 13:29:45 +0800

    Send email fix

--- packages/server/src/utils/sendEmail.ts
+++ packages/server/src/utils/sendEmail.ts
@@ -5,40 +5,35 @@ export const sendEmail = async (
   url: string,
   linkText: string
 ) => {
-  nodemailer.createTestAccount((err1, account) => {
-    if (err1) {
-      console.log(err1);
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: "ln6sjx3zpq2isd6g@ethereal.email",
+      pass: "5SucYP8b43unJr91gu"
     }
-    const transporter = nodemailer.createTransport({
-      host: account.smtp.host,
-      port: account.smtp.port,
-      secure: account.smtp.secure,
-      auth: {
-        user: account.user,
-        pass: account.pass
-      }
-    });
+  });
 
-    const message = {
-      from: "Sender Name <sender@example.com>",
-      to: `Recipient <${recipient}>`,
-      subject: "Nodemailer is unicode friendly ✔",
-      text: "Hello to myself!",
-      html: `<html>
+  const message = {
+    from: "Sender Name <sender@example.com>",
+    to: `Recipient <${recipient}>`,
+    subject: "Nodemailer is unicode friendly ✔",
+    text: "Hello to myself!",
+    html: `<html>
         <body>
-        <p>Testing SparkPost - the world's most awesomest email service!</p>
+        <p>Testing Nodemail</p>
         <a href="${url}">${linkText}</a>
         </body>
         </html>`
-    };
+  };
 
-    transporter.sendMail(message, (err, info) => {
-      if (err) {
-        console.log("Error occurred. " + err.message);
-      }
+  transporter.sendMail(message, (err, info) => {
+    if (err) {
+      console.log("Error occurred. " + err.message);
+    }
 
-      // Preview only available when sending through an Ethereal account
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    });
+    console.log("Message sent: %s", info.messageId);
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   });
 };
