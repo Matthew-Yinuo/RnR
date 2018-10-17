14b345151c9a595f8a8536206bfbe37da695b9b6
Matthew Hoth <matthewhoth@gmail.com>
2018-10-17 23:33:46 +0800

    Create a card to display a listing's information

--- packages/web/src/modules/listing/view/ViewListingConnector.tsx
+++ packages/web/src/modules/listing/view/ViewListingConnector.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { ViewListing } from "@airbnb/controller";
 import { RouteComponentProps, Link } from "react-router-dom";
-
+import { Button } from "antd";
 export class ViewListingConnector extends React.PureComponent<
   RouteComponentProps<{
     listingId: string;
@@ -22,13 +22,57 @@ export class ViewListingConnector extends React.PureComponent<
           }
 
           return (
-            <div>
-              <div>{data.listing.name}</div>
-              <div>
-                <Link to={`/listing/${listingId}/chat`}>chat</Link>
+            <div className="details">
+              <div className="card">
+                <div className="thumbnail">
+                  <img className="left" src={data.listing.pictureUrl} />
+                </div>
+                <div className="right">
+                  <h1>{data.listing.name}</h1>
+                  <div className="author">
+                    <h2> {data.listing.owner.email}</h2>
+                  </div>
+                  <div className="separator" />
+                  <p>
+                    {data.listing.description}
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                  </p>
+                </div>
+                <h5>12</h5>
+                <h6>JANUARY</h6>
+                <ul>
+                  <li className="fas fa-2x">
+                    {data.listing.beds} <i className="fas fa-bed " />
+                  </li>
+                  <li className="fas fa-2x">
+                    {data.listing.guests} <i className="fas fa-user" />
+                  </li>
+                  <li className="fas fa-2x">
+                    <i className="fas fa-dollar-sign ">{data.listing.price}</i>
+                  </li>
+                </ul>
+                <div className="fab">
+                  <i className="fas fa-check fa-3x" />
+                </div>
               </div>
-              <div>
-                <Link to={`/listing/${listingId}/edit`}>edit</Link>
+              <div className="links">
+                <Link to={`/listing/${listingId}/chat`}>
+                  <Button type="primary" block={true} className="chat">
+                    <i className="fas fa-comments fa-2x" /> Chat with the host
+                  </Button>
+                </Link>
+              </div>
+              <div className="links">
+                <Link to={`/listing/${listingId}/edit`}>
+                  <Button type="dashed" block={true} className="chat">
+                    <i className="fas fa-edit fa-2x" /> Make changes to listing
+                  </Button>
+                </Link>
               </div>
             </div>
           );
